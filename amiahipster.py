729d20153b899a90fba37a2f7a0c129669c4d912
natebeacham <nate@beacham.ca>
2013-02-01 18:16:11 -0500

    Initial

---
+++ amiahipster.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+'''
+Python script to determine if you are a hipster
+when writing JavaScript
+
+usgae:
+	amiahipster.py path/to/mycode.js
+'''
+
+import sys
+
+f = open(sys.argv[1])
+
+lines = [line.replace('\n', '') for line in f.readlines()]
+lines = [line for line in lines if line]
+
+semis = filter(lambda line: ';' in line, lines)
+
+if len(semis) < len(lines) / 10:
+	print "Yeah, you're probably a hispter."
+else:
+	print "Nope, you're probably not a hipster."
